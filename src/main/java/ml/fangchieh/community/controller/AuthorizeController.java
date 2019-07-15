2284a829c982b06a62f5165252963ecf67808cd1
fangchieh <358545276@qq.com>
2019-07-15 18:05:58 +0800

    extract github config from code to properties

--- src/main/java/ml/fangchieh/community/controller/AuthorizeController.java
+++ src/main/java/ml/fangchieh/community/controller/AuthorizeController.java
@@ -4,26 +4,34 @@ import ml.fangchieh.community.controller.Provider.Githubprovider;
 import ml.fangchieh.community.controller.dto.AccessTokenDTO;
 import ml.fangchieh.community.controller.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.annotation.Resource;
 
 @Controller
 public class AuthorizeController{
     @Autowired
     private Githubprovider githubprovider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String index(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("f402f9db124aec7e1489");
-        accessTokenDTO.setClient_secret("ca5b28900d11a9967bb875f2840c4d1541ae363b");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubprovider.getAccessToken(accessTokenDTO);
         GithubUser user = githubprovider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
